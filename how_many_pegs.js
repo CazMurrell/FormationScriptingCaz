7dac6e5cf27c92229e73c9763e426fb20f5b6817
CazMurrell <cazetmalo@gmail.com>
2025-06-11 17:06:10 +0200

    third commit

--- how_many_pegs.js
+++ how_many_pegs.js
@@ -1,12 +1,31 @@
-function my_func(){
+//Exercice :show how many pegs have rotation values different from 0
 
-    var list_of_nodes = ["PEG"]
-    var list_of_peg_nodes = node.getNodes(list_of_nodes)
-    var compiled_peg_list = list_of_peg_nodes.join("\n")
+function show_how_many_pegs(){
 
+    var array_of_peg = node.getNodes(["PEG"])
+    MessageLog.trace(array_of_peg)
 
-    MessageBox.information(compiled_peg_list)
-    MessageBox.information("number of peg nodes : "+list_of_peg_nodes.length)
+    var list_of_rotated_pegs = []
+
+    for(var index in array_of_peg){
+
+        var current_peg = array_of_peg[index]
+        var peg_rotation = get_rotation(current_peg)
+        MessageLog.trace(" Rotation of "+current_peg+" = "+peg_rotation)
+
+        if(peg_rotation != 0){
+            MessageLog.trace(current_peg+ " has rotation not equal to 0 ! ")
+            list_of_rotated_pegs.push(current_peg)
+        }
+
+    }
+
+    MessageBox.information(list_of_rotated_pegs.join("\n"))
 
 }
 
+function get_rotation(_peg){ //les fonctions sont même si elle sont notées après l'endroit où elles sont appelées (ici par exemple elle est appelée au dessus)
+
+    var rotation = node.getTextAttr(_peg,frame.current(),"ROTATION.ANGLEZ")
+    return rotation
+}
