7ee44eccd0cf711f8cb35e81d29c2bfaf3d93d5c
CazMurrell <cazetmalo@gmail.com>
2025-06-11 15:28:56 +0200

    first push
    script for peg list

--- how_many_pegs.js
+++ how_many_pegs.js
@@ -1,4 +1,12 @@
-function
+function my_func(){
 
-var list_of_nodes = ["PEG"]
-var
+    var list_of_nodes = ["PEG"]
+    var list_of_peg_nodes = node.getNodes(list_of_nodes)
+    var compiled_peg_list = list_of_peg_nodes.join("\n")
+
+
+    MessageBox.information(list_of_peg_nodes)
+    MessageBox.information("number of peg nodes : "+compiled_peg_list.length)
+
+}
+
