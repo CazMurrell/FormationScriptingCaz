2c55535c4ddc42bcf2e0a36d61052cdad9df5700
CazMurrell <cazetmalo@gmail.com>
2025-06-12 11:23:45 +0200

    peg rotation 90

--- how_many_pegs.js
+++ how_many_pegs.js
@@ -1,3 +1,10 @@
+/*
+include("C:\Users\Travail-6\Documents\Formation_TB_scripting\FormationScriptingCaz\useful_functions.js")
+//ici on peut appeler le chemin d'un autre script, attention de changer les \ par des / en utilisant ctrl+f
+// comme ça on peut appeler la fonction inclue (attention de noter le nom de la fonction, elle ne sera pas proposée ici automatiquement)
+//ça s'appelle un "include"
+
+
 //Exercice :show how many pegs have rotation values different from 0
 
 function show_how_many_pegs(){
@@ -29,3 +36,26 @@ function get_rotation(_peg){ //les fonctions sont même si elle sont notées apr
     var rotation = node.getTextAttr(_peg,frame.current(),"ROTATION.ANGLEZ")
     return rotation
 }
+
+>>>> le quizz d'Alex ! <<<< https://github.com/alarigger/AL_ScriptingCourseHelp
+
+*/
+
+//SET ROTATION
+
+//https://docs.toonboom.com/help/harmony-24/scripting/script/classnode.html ()
+
+function rotate_peg_90_degrees (_peg){
+var selected_node = selection.selectedNode(0) //selection. c'est un outil TB pour indiquer quel node est sélectionné dans la nodeview on pourrait aussi mettre selectedNodes()[0] (ça choisit le premier dans un array de nodes selectionés)
+    MessageBox.information(selected_node)
+
+
+}
+
+
+function set_rotation(_peg,_angle){
+    node.setTextAttr(_peg,"ROTATION.ANGLEZ",frame.current(),"90")
+}
+
+
+    
