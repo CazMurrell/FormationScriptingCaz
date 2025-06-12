69bc0474eaa8348f1ca15bea597310ee244a9fa5
CazMurrell <cazetmalo@gmail.com>
2025-06-12 12:18:46 +0200

    commit5

--- how_many_pegs.js
+++ how_many_pegs.js
@@ -39,23 +39,48 @@ function get_rotation(_peg){ //les fonctions sont même si elle sont notées apr
 
 >>>> le quizz d'Alex ! <<<< https://github.com/alarigger/AL_ScriptingCourseHelp
 
-*/
 
 //SET ROTATION
 
 //https://docs.toonboom.com/help/harmony-24/scripting/script/classnode.html ()
+*/
+function get_rotation(_peg){ //les fonctions sont même si elle sont notées après l'endroit où elles sont appelées (ici par exemple elle est appelée au dessus)
 
-function set_rotation (_peg){
-var selected_node = selection.selectedNode(0) //selection. c'est un outil TB pour indiquer quel node est sélectionné dans la nodeview on pourrait aussi mettre selectedNodes()[0] (ça choisit le premier dans un array de nodes selectionés)
-    MessageBox.information(selected_node)
-
-
+    var rotation_string = node.getTextAttr(_peg,frame.current(),"ROTATION.ANGLEZ") // attention ça donne la rotation mais en string, pas en int
+    var float_rotation = parseFloat(rotation_string) // parseFloat() change un chiffre en un entier
+    return float_rotation
 }
 
 
-function rotate_peg_90_degrees(_peg){
+function set_rotation (_peg,_angle){
+    node.setTextAttr(_peg,"ROTATION.ANGLEZ",frame.current(),_angle)   
+    
+}
+
+/*
+function rotate_peg_90_degrees(){
+    var selected_node = selection.selectedNode(0) //selection. c'est un outil TB pour indiquer quel node est sélectionné dans la nodeview on pourrait aussi mettre selectedNodes()[0] (ça choisit le premier dans un array de nodes selectionés)
     node.setTextAttr(_peg,"ROTATION.ANGLEZ",frame.current(),"90")
 }
+*/
+
+// ici une version plus intentionelle où on isole la valeur d'angle qu'on change
+function rotate_peg_90_degrees(){
+    var selected_node = selection.selectedNode(0) 
+    MessageBox.information(selected_node)
+    var angle = 90
+    node.setTextAttr(selected_node,angle)
+}
+
+function rotate_peg_add_90_degrees(){
+    var selected_peg = selection.selectedNode(0)
+    MessageBox.information(selected_node)
+    var old_angle = get_rotation(selected_node)
+    MessageBox.information(old_angle)
+    var new_angle = old angle + 90
+    set_rotation(selected_peg,new_angle)
+}
+
 
 
     
